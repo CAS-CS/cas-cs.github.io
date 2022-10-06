ed68800c6237a42a654ed0638acd28d46d4d625c
prateekrajgautam <prateekrajgautam@gmail.com>
2022-10-06 19:49:20 +0530

    update sorting of folders and hide folders starting with .

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ window.$$ = GeneratorWebHelper()
 $.init()
 
 //STYLE
-getfile("/style.scss",(siteStyleSCSS)=>{loadscss(siteStyleSCSS)})
+getfile("/style.scss", (siteStyleSCSS) => { loadscss(siteStyleSCSS) })
 //your app logic
 append(app, "", "over") /* reset app */
 append(app, gen(header, "header", "", 'header'));
-append(header, gen(h3, 'heading','Lecture Server','title'))
+append(header, gen(h3, 'heading', 'Lecture Server', 'title'))
 var loc = window.location.href
 
 var printLocation = loc.replaceAll('https://', "").replaceAll('http://', "")
@@ -18,41 +18,44 @@ append("#location", "", 'over')
 var path = window.location.protocol + "//"
 printLocation.split("/").forEach(l => {
     if (l.length > 0) {
-        path = path +"/"+ l
+        path = path + "/" + l
         append("#location", gen(a, '', l, 'pathNavigator', path + "/"))
         append("#location", gen(span, '', "  ", 'spacer'))
 
     }
 
 })
-append(app, gen(main, "appmain", gen(h1,'FileBrowser',"Directory List"), 'appmain container'));
+append(app, gen(main, "appmain", gen(h1, 'FileBrowser', "Directory List"), 'appmain container'));
 append(app, gen(footer, "appfooter", "", 'footer'));
 append(appmain, gen(section, "hero", "", "hero"))
 // load(["./header.js", "./script.js", "./footer.js","./style.css"])// load js and css files
 // append(hero, [gen(h1, "", "Hello GeneratorJs"), gen(p, "", "Yes, just Three functions for frontend."), gen(span, "", ["gen", "append", "load"]), gen(p, "", "And Three Optional functions."), gen(span, "", ["log", "getfile", "loadscss"])])        
 
-append(main,gen(div,'directoryGrid',"","dirGrid"))
+append(main, gen(div, 'directoryGrid', "", "dirGrid"))
 
-getfile(window.location.href + "list.txt",filelist=>{filelist.split("\n").forEach(link => {
+getfile(window.location.href + "list.txt", filelist => {
+    filelist.split("\n").sort().forEach(link => {
+        if (link[2] != '.') {
             var linkname = link.replaceAll("./", "").replaceAll("/", " / ")
-            if (link.length>0 && link!='./'){
-            append(directoryGrid, gen(a, "", linkname, 'folderLinks', link))
+            if (link.length > 0 && link != './') {
+                append(directoryGrid, gen(a, "", linkname, 'folderLinks', link))
             }
-        });
+        }
+    });
 
 
 
-        append(main,gen(button,"reload","Reload","reloadPage",{"onclick":"reloadPage()"}))
+    append(main, gen(button, "reload", "Reload", "reloadPage", { "onclick": "reloadPage()" }))
 })
 
 
-        
-function reloadPage(){
-sessionStorage.clear()
-localStorage.clear()    
-var url =window.location.href                
-console.log(url)
-window.location.href=url
+
+function reloadPage() {
+    sessionStorage.clear()
+    localStorage.clear()
+    var url = window.location.href
+    console.log(url)
+    window.location.href = url
 }
 
 
