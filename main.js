622cc481a6eb512d2e2829ba90d02816df6b7129
prateekrajgautam <prateekrajgautam@gmail.com>
2023-03-28 11:08:14 +0545

    update

--- main.js
+++ main.js
@@ -4,160 +4,6 @@ $.init()
 
 //STYLE
 
-load('/style.scss')
-//your app logic
-append(app, "", "over") /* reset app */
-append(app, gen(header, "header", "", 'header'));
-append(header, gen(h3, 'heading', 'Lecture Server', 'title'))
-var loc = window.location.href
-
-var printLocation = loc.replaceAll('https://', "").replaceAll('http://', "")
-append(header, gen(div, 'location', printLocation))
-
-
-append("#location", "", 'over')
-var path = window.location.protocol + "//"
-printLocation.split("/").forEach(l => {
-    if (l.length > 0) {
-        path = path + "/" + l
-        append("#location", gen(a, '', l, 'pathNavigator', path + "/"))
-        append("#location", gen(span, '', "  ", 'spacer'))
-
-    }
-
-})
-append(app, gen(main, "appmain", gen(h1, 'FileBrowser', "Directory List"), 'appmain container'));
-append(app, gen(footer, "appfooter", "", 'footer'));
-append(appmain, gen(section, "hero", "", "hero"))
-// load(["./header.js", "./script.js", "./footer.js","./style.css"])// load js and css files
-// append(hero, [gen(h1, "", "Hello GeneratorJs"), gen(p, "", "Yes, just Three functions for frontend."), gen(span, "", ["gen", "append", "load"]), gen(p, "", "And Three Optional functions."), gen(span, "", ["log", "getfile", "loadscss"])])        
-
-append(main, gen(div, 'directoryGrid', "", "dirGrid"))
-
-var currentLocation = window.location.href
-// fileListUrl=currentLocation+"/"+'list.txt'
-fileListUrl = currentLocation + 'list.txt'
-// log(fileListUrl)
-var notebook = currentLocation + '/slide.ipynb'
-var slideUrl = currentLocation + '/slide.md'
-try {
-    parseNotebook(notebook)
-} catch (error) {
-    parseSlide(slideUrl)
-}
-loadButtonToFiles(fileListUrl)
-function loadButtonToFiles(fileListUrl) {
-
-    getfile(fileListUrl, filelist => {
-
-        filelist.split("\n").sort().forEach(link => {
-            var url = currentLocation + link.replaceAll('./', '')
-            link = link.replaceAll("\t", "")
-
-            if (link[2] != '.' && !link.includes(".md")) {
-                var linkname = link.replaceAll("./", "").replaceAll("/", " / ").replaceAll("-", " ")
-                if (link.length > 0 && link != './') {
-                    var redirect = currentLocation + link.replaceAll('./', '') + "/"
-                    // redirect = redirect.replaceAll(" ", "")
-                    // log(redirect)
-                    // append(directoryGrid, gen(a, "", linkname, 'folderLinks', link))
-                    append(directoryGrid, gen(a, "", linkname, 'folderLinks', redirect))
-                }
-            }
-
-            //for markdown files
-            if (link[2] != '.' && link.includes(".md")) {
-                var linkname = link.replaceAll("./", "").replaceAll("/", " / ").replaceAll("-", " ").replaceAll(".md", "")
-                if (link.length > 0 && link != './') {
-                    var redirect = currentLocation + link.replaceAll('./', '') + "/"
-                    // redirect = redirect.replaceAll(" ", "")
-                    // log(redirect)
-                    // append(directoryGrid, gen(a, "", linkname, 'folderLinks', link))
-                    append(directoryGrid, gen(a, url, linkname, 'slideLinks', { "onclick": `parseSlide(\`${url}\`)` }))
-                }
-            }
-        });
-
-
-
-        append(main, gen(button, "reload", "Reload", "button,reloadPage", { "onclick": "reloadPage()" }))
-        append(main, gen(button, "open", "Open", "button,openFile", { "onclick": "openFile()" }))
-    })
-}
-
-const openFile = () => {
-    // var file = window.FileReader || window.webkitFileReader;
-
-}
-
-
-function parseSlide(link) {
-    // log(link)
-    getfile(link, md => {
-        append(header, gen(a, "Back", "Back", "pathNavigator", { "onclick": "reloadPage()", "tabindex": 0 }))
-
-        append(appmain, gen(div, "slideroot", "", 'slideroot'), "replace")
-        append(slideroot, gen("aside", "sideBar", ""))
-        append(sideBar, gen(div, "slidenav", gen(h3, "", "Navigator")))
-        append(slidenav, gen(ul, "slidenavlist", "", "slidenavlist"))
-
-        var html = md.split("---")
-
-        for (var i = 0; i < html.length; i++) {
-            var h = html[i]
-            if (h.length > 0) {
-                parsemd(h, H => {
-                    append(slideroot, gen(section, `slide${i}`, H, "slide"))
-                    if (i != 0 && i != html.length - 1) {
-                        append(get(".slide")[i], gen(span, "", `${i + 1}/${html.length}`, "slideCount"))
-                        append(slidenavlist, gen(li, "", gen(a, "", `Slide ${i + 1}`, "slideNavLink", { "onclick": `slide${i}.scrollIntoView()` })))
-                    }
-                })
-            }
-        }
-        MathJax.typesetClear()
-        MathJax.typeset("slideroot")
-        MathJax.typeset()
-        GeneratorWebHelper().addCopyButton()
-
-    })
-    loadscss(slideScss)
-}
-
-
-function parseNotebook(link) {
-    // log(link)
-    getfile(link, md => {
-        append(header, gen(a, "Back", "Back", "pathNavigator", { "onclick": "reloadPage()", "tabindex": 0 }))
-
-        append(appmain, gen(div, "slideroot", "", 'slideroot'), "replace")
-        append(slideroot, gen("aside", "sideBar", ""))
-        append(sideBar, gen(div, "slidenav", gen(h3, "", "Navigator")))
-        append(slidenav, gen(ul, "slidenavlist", "", "slidenavlist"))
-
-        var html = md.split("---")
-
-        for (var i = 0; i < html.length; i++) {
-            var h = html[i]
-            if (h.length > 0) {
-                parsemd(h, H => {
-                    append(slideroot, gen(section, `slide${i}`, H, "slide"))
-                    if (i != 0 && i != html.length - 1) {
-                        append(get(".slide")[i], gen(span, "", `${i + 1}/${html.length}`, "slideCount"))
-                        append(slidenavlist, gen(li, "", gen(a, "", `Slide ${i + 1}`, "slideNavLink", { "onclick": `slide${i}.scrollIntoView()` })))
-                    }
-                })
-            }
-        }
-        MathJax.typesetClear()
-        MathJax.typeset("slideroot")
-        MathJax.typeset()
-        GeneratorWebHelper().addCopyButton()
-
-    })
-    loadscss(slideScss)
-}
-
 var slideScss = `
 @import url('https://fonts.googleapis.com/css2?family=Cantarell&display=swap');
 :root{
@@ -369,6 +215,163 @@ var slideScss = `
   
 
 `
+
+
+
+load('/style.scss')
+//your app logic
+append(app, "", "over") /* reset app */
+append(app, gen(header, "header", "", 'header'));
+append(header, gen(h3, 'heading', 'Lecture Server', 'title'))
+var loc = window.location.href
+
+var printLocation = loc.replaceAll('https://', "").replaceAll('http://', "")
+append(header, gen(div, 'location', printLocation))
+
+
+append("#location", "", 'over')
+var path = window.location.protocol + "//"
+printLocation.split("/").forEach(l => {
+    if (l.length > 0) {
+        path = path + "/" + l
+        append("#location", gen(a, '', l, 'pathNavigator', path + "/"))
+        append("#location", gen(span, '', "  ", 'spacer'))
+
+    }
+
+})
+append(app, gen(main, "appmain", gen(h1, 'FileBrowser', "Directory List"), 'appmain container'));
+append(app, gen(footer, "appfooter", "", 'footer'));
+append(appmain, gen(section, "hero", "", "hero"))
+// load(["./header.js", "./script.js", "./footer.js","./style.css"])// load js and css files
+// append(hero, [gen(h1, "", "Hello GeneratorJs"), gen(p, "", "Yes, just Three functions for frontend."), gen(span, "", ["gen", "append", "load"]), gen(p, "", "And Three Optional functions."), gen(span, "", ["log", "getfile", "loadscss"])])        
+
+append(main, gen(div, 'directoryGrid', "", "dirGrid"))
+
+var currentLocation = window.location.href
+// fileListUrl=currentLocation+"/"+'list.txt'
+fileListUrl = currentLocation + 'list.txt'
+// log(fileListUrl)
+var notebook = currentLocation + '/slide.ipynb'
+var slideUrl = currentLocation + '/slide.md'
+try {
+    parseNotebook(notebook)
+} catch (error) {
+    parseSlide(slideUrl)
+}
+loadButtonToFiles(fileListUrl)
+function loadButtonToFiles(fileListUrl) {
+
+    getfile(fileListUrl, filelist => {
+
+        filelist.split("\n").sort().forEach(link => {
+            var url = currentLocation + link.replaceAll('./', '')
+            link = link.replaceAll("\t", "")
+
+            if (link[2] != '.' && !link.includes(".md")) {
+                var linkname = link.replaceAll("./", "").replaceAll("/", " / ").replaceAll("-", " ")
+                if (link.length > 0 && link != './') {
+                    var redirect = currentLocation + link.replaceAll('./', '') + "/"
+                    // redirect = redirect.replaceAll(" ", "")
+                    // log(redirect)
+                    // append(directoryGrid, gen(a, "", linkname, 'folderLinks', link))
+                    append(directoryGrid, gen(a, "", linkname, 'folderLinks', redirect))
+                }
+            }
+
+            //for markdown files
+            if (link[2] != '.' && link.includes(".md")) {
+                var linkname = link.replaceAll("./", "").replaceAll("/", " / ").replaceAll("-", " ").replaceAll(".md", "")
+                if (link.length > 0 && link != './') {
+                    var redirect = currentLocation + link.replaceAll('./', '') + "/"
+                    // redirect = redirect.replaceAll(" ", "")
+                    // log(redirect)
+                    // append(directoryGrid, gen(a, "", linkname, 'folderLinks', link))
+                    append(directoryGrid, gen(a, url, linkname, 'slideLinks', { "onclick": `parseSlide(\`${url}\`)` }))
+                }
+            }
+        });
+
+
+
+        append(main, gen(button, "reload", "Reload", "button,reloadPage", { "onclick": "reloadPage()" }))
+        append(main, gen(button, "open", "Open", "button,openFile", { "onclick": "openFile()" }))
+    })
+}
+
+const openFile = () => {
+    // var file = window.FileReader || window.webkitFileReader;
+
+}
+
+
+function parseSlide(link) {
+    // log(link)
+    getfile(link, md => {
+        append(header, gen(a, "Back", "Back", "pathNavigator", { "onclick": "reloadPage()", "tabindex": 0 }))
+
+        append(appmain, gen(div, "slideroot", "", 'slideroot'), "replace")
+        append(slideroot, gen("aside", "sideBar", ""))
+        append(sideBar, gen(div, "slidenav", gen(h3, "", "Navigator")))
+        append(slidenav, gen(ul, "slidenavlist", "", "slidenavlist"))
+
+        var html = md.split("---")
+
+        for (var i = 0; i < html.length; i++) {
+            var h = html[i]
+            if (h.length > 0) {
+                parsemd(h, H => {
+                    append(slideroot, gen(section, `slide${i}`, H, "slide"))
+                    if (i != 0 && i != html.length - 1) {
+                        append(get(".slide")[i], gen(span, "", `${i + 1}/${html.length}`, "slideCount"))
+                        append(slidenavlist, gen(li, "", gen(a, "", `Slide ${i + 1}`, "slideNavLink", { "onclick": `slide${i}.scrollIntoView()` })))
+                    }
+                })
+            }
+        }
+        MathJax.typesetClear()
+        MathJax.typeset("slideroot")
+        MathJax.typeset()
+        GeneratorWebHelper().addCopyButton()
+
+    })
+    loadscss(slideScss)
+}
+
+
+function parseNotebook(link) {
+    // log(link)
+    getfile(link, md => {
+        append(header, gen(a, "Back", "Back", "pathNavigator", { "onclick": "reloadPage()", "tabindex": 0 }))
+
+        append(appmain, gen(div, "slideroot", "", 'slideroot'), "replace")
+        append(slideroot, gen("aside", "sideBar", ""))
+        append(sideBar, gen(div, "slidenav", gen(h3, "", "Navigator")))
+        append(slidenav, gen(ul, "slidenavlist", "", "slidenavlist"))
+
+        var html = md.split("---")
+
+        for (var i = 0; i < html.length; i++) {
+            var h = html[i]
+            if (h.length > 0) {
+                parsemd(h, H => {
+                    append(slideroot, gen(section, `slide${i}`, H, "slide"))
+                    if (i != 0 && i != html.length - 1) {
+                        append(get(".slide")[i], gen(span, "", `${i + 1}/${html.length}`, "slideCount"))
+                        append(slidenavlist, gen(li, "", gen(a, "", `Slide ${i + 1}`, "slideNavLink", { "onclick": `slide${i}.scrollIntoView()` })))
+                    }
+                })
+            }
+        }
+        MathJax.typesetClear()
+        MathJax.typeset("slideroot")
+        MathJax.typeset()
+        GeneratorWebHelper().addCopyButton()
+
+    })
+    loadscss(slideScss)
+}
+
 function reloadPage() {
     sessionStorage.clear()
     localStorage.clear()
