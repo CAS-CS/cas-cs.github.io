53c64545fc1d2aee5611e0fd59a0950f582519cf
prateekrajgautam <prateekrajgautam@gmail.com>
2023-09-30 11:07:33 +0530

    parcel init but not w

--- edit.sh
+++ edit.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+# npm init -y
+# npm i --save-dev parcel
+# "dev":"parcel ./index.html",
+# "build":"parcel build ./index.html"
 NIXPKGS_ALLOW_UNFREE=1
-nix-shell --sandbox --pure -p vscode firefox-devedition --run "code --install-extension ritwickdey.liveserver && code . && firefox"
+#nix-shell --sandbox --pure -p vscode firefox-devedition nodejs pkgs.nodePackages.parcel --run "code --install-extension ritwickdey.liveserver && code . && firefox 127.0.0.1:1234 && npm run dev"
+
+nix-shell --sandbox --pure -p vscode firefox-devedition nodejs pkgs.nodePackages.parcel --run "code --install-extension ritwickdey.liveserver && code . && firefox 127.0.0.1:5501"
