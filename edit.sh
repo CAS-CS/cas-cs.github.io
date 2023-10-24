ac04177a0a7d292a6bc2f61a8ac9e1a287e33217
prateekrajgautam <prateekrajgautam@gmail.com>
2023-10-24 13:28:15 +0530

    use browser synch

--- edit.sh
+++ edit.sh
@@ -9,4 +9,5 @@ NIXPKGS_ALLOW_UNFREE=1
 
 #nix-shell --sandbox --pure -p vscode firefox-devedition nodejs pkgs.nodePackages.parcel --run "code --install-extension ritwickdey.liveserver && code . && firefox 127.0.0.1:5501"
 #
+# nix-shell --sandbox --pure -p vscodium firefox-devedition nodejs pkgs.nodePackages.nodemon pkgs.nodePackages.parcel --run "codium . && npm i && npm run dev & firefox 127.0.0.1:5501"
 nix-shell --sandbox --pure -p vscodium firefox-devedition nodejs pkgs.nodePackages.nodemon pkgs.nodePackages.parcel --run "codium . && npm i && npm run dev & firefox 127.0.0.1:5501"
