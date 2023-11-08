5726259307f6af27097a85bbd07c716c6888b999
prateekrajgautam <prateekrajgautam@gmail.com>
2023-11-08 16:52:52 +0530

    removed Lecture to seprate Repo

--- Lectures/.Old/RM/generateDir_Md_ipynbList.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#find ./ -name "*" -type d > list.txt
-find ./ -maxdepth 1 -name "*" -type d >list.txt
-find ./*.md -maxdepth 1 -name "*" >>list.txt
-find ./*.ipynb -maxdepth 1 -name "*" >>list.txt
